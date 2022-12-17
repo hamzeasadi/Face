e23707d66a6a7793dbfb74726f578a21c44d4d27
hamzeasadi <hamzeasadi1@gmail.com>
2022-12-17 20:59:57 +0000

    edit code

--- datasetup.py
+++ datasetup.py
@@ -56,34 +56,34 @@ def split_data():
 
 
 
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Resize((224,224)),
-        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
-        transforms.RandomRotation(5, interpolation=transforms.InterpolationMode.NEAREST,expand=False, center=None),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize((224,224)),
-       transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
-        transforms.RandomRotation(5, interpolation=transforms.InterpolationMode.NEAREST,expand=False, center=None),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    ]),
-}
-# data_dir = '/content/drive/MyDrive/AttendanceCapturingSystem/data/'
-data_dir = os.path.join(cfg.paths['dataset'])
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x],
-                                              batch_size=32, 
-                                             shuffle=True)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train','val']}
-class_names = image_datasets['train'].classes
+# data_transforms = {
+#     'train': transforms.Compose([
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Resize((224,224)),
+#         transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
+#         transforms.RandomRotation(5, interpolation=transforms.InterpolationMode.NEAREST,expand=False, center=None),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ]),
+#     'val': transforms.Compose([
+#         transforms.ToTensor(),
+#         transforms.Resize((224,224)),
+#        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
+#         transforms.RandomRotation(5, interpolation=transforms.InterpolationMode.NEAREST,expand=False, center=None),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+#     ]),
+# }
+# # data_dir = '/content/drive/MyDrive/AttendanceCapturingSystem/data/'
+# data_dir = os.path.join(cfg.paths['dataset'])
+# image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+#                                           data_transforms[x])
+#                   for x in ['train', 'val']}
+# dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x],
+#                                               batch_size=32, 
+#                                              shuffle=True)
+#               for x in ['train', 'val']}
+# dataset_sizes = {x: len(image_datasets[x]) for x in ['train','val']}
+# class_names = image_datasets['train'].classes
 
 
 def imshow(inp, title=None):
